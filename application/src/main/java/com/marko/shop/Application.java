81747cb44164a114bdbe775bfa8862f4442a84a9
Marko Vuckovic <marvuchko@gmail.com>
2019-06-14 13:43:20 +0200

    Initial architecture setup

---
+++ application/src/main/java/com/marko/shop/Application.java
@@ -0,0 +1,22 @@
+package com.marko.shop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Shopping application has started working!");
+    }
+}
---
+++ run.sh
@@ -0,0 +1,5 @@
+# Stop the containers in case they have been up and running
+docker-compose down
+
+# Start the system
+docker-compose up
