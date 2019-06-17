aef615f1e88b5390bdfadc7552a709c243e448c0
Marko Vuckovic <marvuchko@gmail.com>
2019-06-17 08:19:04 +0200

    run-with-docker.sh updated.

--- run-with-docker.sh
+++ run-with-docker.sh
@@ -4,7 +4,7 @@
 # Stop the containers in case they have been up and running
 docker-compose down
 
-# Start the system
+# Start the containers
 docker-compose up
 
 # Performe SonarQube code analysis
@@ -13,6 +13,7 @@ docker-compose up
 # Check your browser:
 # REST API documentation:     http://localhost:28003/api/swagger-ui.html
 # H2 database administration: http://localhost:28003/api/h2
+#                             database: shop
 #                             username: marko
 #                             password: marko
 # SonarQube:                  http://localhost:28004
